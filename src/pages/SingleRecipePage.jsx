8be81c3b1da96e36b40402c3db4f718f6842ee32
Chantal Verschueren <cverschueren34@gmail.com>
2023-08-21 13:43:54 +0200

    Create nutrient variables

--- src/pages/SingleRecipePage.jsx
+++ src/pages/SingleRecipePage.jsx
@@ -6,11 +6,17 @@ import {
   Stack,
   Heading,
   Text,
-  Flex,
   Button,
 } from "@chakra-ui/react";
 
 export const SingleRecipePage = ({ item, clickFn }) => {
+  const calories = Math.trunc(item.totalNutrients.ENERC_KCAL.quantity);
+  const carbs = Math.trunc(item.totalNutrients.CHOCDF.quantity);
+  const protein = Math.trunc(item.totalNutrients.PROCNT.quantity);
+  const fat = Math.trunc(item.totalNutrients.FAT.quantity);
+  const cholesterol = Math.trunc(item.totalNutrients.CHOLE.quantity);
+  const sodium = Math.trunc(item.totalNutrients.NA.quantity);
+
   return (
     <Center bgColor="blue.200" h="100vh" flexDirection="column">
       <Card
@@ -32,17 +38,23 @@ export const SingleRecipePage = ({ item, clickFn }) => {
           />
           <Stack mt="6" spacing="3">
             <Heading size="md">{item.hits.recipe.label}</Heading>
-            <Text color="blue.600">{item.hits.recipe.mealType}
-            {item.hits.recipe.dishType}
-            {item.hits.recipe.totalTime}
-            {item.hits.recipe.dietLabels}
-            {item.hits.recipe.healthLabels}
-            {item.hits.recipe.cautions}
-            {item.hits.recipe.ingredientLines}
-            {item.hits.recipe.dishType}
-            {item.hits.recipe.totalNutrients->kcal proteine fat carbs cholesterol sodium}
+            <Text color="blue.600">
+              {item.hits.recipe.mealType}
+              {item.hits.recipe.dishType}
+              {item.hits.recipe.totalTime}
+              {item.hits.recipe.dietLabels}
+              {item.hits.recipe.healthLabels}
+              {item.hits.recipe.cautions}
+              {item.hits.recipe.ingredientLines}
+              {item.hits.recipe.dishType}
+              <Text>total nutrients:</Text>
+              <li>{calories} CALORIES</li>
+              <li>{carbs} g CARBS</li>
+              <li>{protein} g PROTEIN</li>
+              <li>{fat} g FAT</li>
+              <li>{cholesterol} mg CHOLESTEROL</li>
+              <li>{sodium} mg SODIUM</li>
             </Text>
-            
 
             <Button color="blue.600" w="fit-content" onClick={() => clickFn()}>
               Back to overview
