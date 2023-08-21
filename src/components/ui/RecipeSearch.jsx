23bf6ec978ee1a59164b7e64589c9a7276fb6633
Chantal Verschueren <cverschueren34@gmail.com>
2023-08-21 10:38:17 +0200

    Create RecipeSearch component

---
+++ src/components/ui/RecipeSearch.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { availableRecipes } from "../../utils/data";
+import { TextInput } from "./TextInput";
+import { RecipeListPage } from "../../pages/RecipeListPage";
+
+export const RecipeSearch = ({ clickFn }) => {
+  const [searchField, setSearchField] = useState(" ");
+
+  const matchedRecipes = availableRecipes.filter((recipe) => {
+    return recipe.label.toLowerCase().includes(searchField.toLowerCase());
+  });
+
+  const handleChange = (event) => {
+    setSearchField(event.target.value);
+  };
+
+  return (
+    <>
+      <label>Search for drinks:</label>
+      <TextInput changeFn={handleChange} w={200} mb={8} />
+      <RecipeListPage clickFn={clickFn} drinks={matchedRecipes} />
+    </>
+  );
+};
