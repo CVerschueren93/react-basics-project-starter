701029f89e4d3514a1f18d522f698b8962752555
Chantal Verschueren <cverschueren34@gmail.com>
2023-08-22 11:23:43 +0200

    Import Flex and Tag

--- src/components/ui/RecipeCard.jsx
+++ src/components/ui/RecipeCard.jsx
@@ -1,4 +1,15 @@
-import { Card, Image, CardBody, Stack, Heading, Text } from "@chakra-ui/react";
+import {
+  Card,
+  Center,
+  Image,
+  CardBody,
+  Stack,
+  Heading,
+  Text,
+  Flex,
+} from "@chakra-ui/react";
+
+import { Tag } from "../Tag";
 
 export const RecipeCard = ({ item, clickFn }) => {
   const info = item.recipe;
@@ -36,34 +47,38 @@ export const RecipeCard = ({ item, clickFn }) => {
     }
   };
   return (
-    <Card
-      borderRadius="xl"
-      borderStyle={"solid"}
-      borderWidth={3}
-      w="sm"
-      h="30rem"
-      onClick={() => clickFn(item)}
-      cursor="pointer"
-      _hover={{ transform: "scale(1.01)" }}
-    >
-      <CardBody>
-        <Image
-          h={64}
-          w="sm"
-          src={item.image}
-          borderRadius="md"
-          borderStyle={"solid"}
-          borderWidth={2}
-          borderColor={"black"}
-        />
-        <Stack mt="6" spacing="3">
-          <Heading size="md">{item.label}</Heading>
-          <Text color="blue.600">
-            {dietLabels()},{cautions()},{item.mealType},{item.dishType},{" "}
-            {veganVegetarian()},
-          </Text>
-        </Stack>
-      </CardBody>
-    </Card>
+    <Center>
+      <Card
+        borderRadius="xl"
+        borderStyle={"solid"}
+        borderWidth={3}
+        w="sm"
+        h="30rem"
+        onClick={() => clickFn(item)}
+        cursor="pointer"
+        _hover={{ transform: "scale(1.01)" }}
+      >
+        <CardBody>
+          <Image
+            h={64}
+            w="sm"
+            src={item.image}
+            borderRadius="md"
+            borderStyle={"solid"}
+            borderWidth={2}
+            borderColor={"black"}
+          />
+          <Stack mt="6" spacing="3">
+            <Heading size="md">{item.label}</Heading>
+            <Text color="blue.600">
+              <Flex>
+                <Tag>{dietLabels()},</Tag>
+              </Flex>
+              {cautions()},{item.mealType},{item.dishType}, {veganVegetarian()},
+            </Text>
+          </Stack>
+        </CardBody>
+      </Card>
+    </Center>
   );
 };
