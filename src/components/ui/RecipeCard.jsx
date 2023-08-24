6f25ddd990ec63a4049d81144e60b4b0824469d2
Chantal Verschueren <cverschueren34@gmail.com>
2023-08-24 11:57:26 +0200

    Remove Chakra and change structure

--- src/components/ui/RecipeCard.jsx
+++ src/components/ui/RecipeCard.jsx
@@ -1,20 +1,7 @@
-import {
-  Card,
-  Center,
-  Image,
-  CardBody,
-  Stack,
-  Heading,
-  Text,
-  Flex,
-} from "@chakra-ui/react";
+export const RecipeCard = ({ selectedRecipe, onClick }) => {
+  const info = selectedRecipe.recipe;
 
-import { Tag } from "../Tag";
-
-export const RecipeCard = ({ item, clickFn }) => {
-  const info = item.recipe;
-
-  const veganVegetarian = () => {
+  const veganOrVegetarian = () => {
     if (info.healthLabels.includes("Vegan")) {
       return "Vegan";
     } else if (info.healthLabels.includes("Vegetarian")) {
@@ -40,46 +27,61 @@ export const RecipeCard = ({ item, clickFn }) => {
 
       return (
         <>
-          Cautions:
+          <p>Cautions:</p>
           <ul>{cautionList}</ul>
         </>
       );
     }
   };
   return (
-    <Center>
-      <Card
-        borderRadius="xl"
-        borderStyle={"solid"}
-        borderWidth={3}
-        w="sm"
-        h="30rem"
-        onClick={() => clickFn(info)}
-        cursor="pointer"
-        _hover={{ transform: "scale(1.01)" }}
-      >
-        <CardBody>
-          <Image
-            h={64}
-            w="sm"
-            src={item.recipe.image}
-            borderRadius="md"
-            borderStyle={"solid"}
-            borderWidth={2}
-            borderColor={"black"}
-          />
-          <Stack mt="6" spacing="3">
-            <Heading size="md">{item.recipe.label}</Heading>
-            <Text color="blue.600">
-              <Flex>
-                <Tag>{dietLabels()},</Tag>
-              </Flex>
-              {cautions()},{item.recipe.mealType},{item.recipe.dishType},
-              {veganVegetarian()},
-            </Text>
-          </Stack>
-        </CardBody>
-      </Card>
-    </Center>
+    <>
+      <button onClick={() => onClick(selectedRecipe)}>
+        <div>
+          <h2>{info.label}</h2>
+          <img src={info.image}></img>
+          <p>Meal type: {info.mealType}</p>
+          <p>Dish type: {info.dishType}</p>
+          {dietLabels()}
+          {cautions()}
+          {veganOrVegetarian()}
+        </div>
+      </button>
+    </>
   );
 };
+// <Center>
+// <Card
+// borderRadius="xl"
+//borderStyle={"solid"}
+//borderWidth={3}
+//w="sm"
+//h="30rem"
+//onClick={() => clickFn(info)}
+//cursor="pointer"
+//_hover={{ transform: "scale(1.01)" }}
+//>
+//<CardBody>
+//<Image
+//h={64}
+//w="sm"
+//src={item.recipe.image}
+//borderRadius="md"
+//borderStyle={"solid"}
+//borderWidth={2}
+//borderColor={"black"}
+///>
+//<Stack mt="6" spacing="3">
+//<Heading size="md">{item.recipe.label}</Heading>
+//<Text color="blue.600">
+//<Flex>
+//<Tag>{dietLabels()},</Tag>
+//</Flex>
+//{cautions()},{item.recipe.mealType},{item.recipe.dishType},
+//{veganVegetarian()},
+//</Text>
+//</Stack>
+//</CardBody>
+//</Card>
+//</Center>
+//);
+//};
