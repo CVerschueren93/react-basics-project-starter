6945eed3322cd277896e1c52969f75c0e0a3412d
Chantal Verschueren <cverschueren34@gmail.com>
2023-08-24 11:58:17 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App";
+import { RecipeListPage } from "./pages/RecipeListPage";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <RecipeListPage />
     </ChakraProvider>
   </React.StrictMode>
 );
